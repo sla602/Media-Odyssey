17c2b802a67467c2b336bcd92c8991cfc338c641
Edmond Yoong <edmond_yoong@sfu.ca>
2026-04-05 01:39:57 -0700

    fix: Duplicate session bug & auth spinner not disappearing automatically

--- src/main/java/com/mo/mediaodyssey/auth/config/SessionConfig.java
+++ src/main/java/com/mo/mediaodyssey/auth/config/SessionConfig.java
@@ -3,6 +3,9 @@ package com.mo.mediaodyssey.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.session.config.SessionRepositoryCustomizer;
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
+import org.springframework.session.jdbc.PostgreSqlJdbcIndexedSessionRepositoryCustomizer;
 
 @Configuration
 public class SessionConfig {
@@ -10,5 +13,10 @@ public class SessionConfig {
     @Bean
     public HttpSessionEventPublisher httpSessionEventPublisher() {
         return new HttpSessionEventPublisher();
+    }
+
+    @Bean
+    public SessionRepositoryCustomizer<JdbcIndexedSessionRepository> postgresJdbcSessionCustomizer() {
+        return new PostgreSqlJdbcIndexedSessionRepositoryCustomizer();
     }
 }
