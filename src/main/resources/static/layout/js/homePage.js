a7f6b8ae7680a4e5b523434eb62a641c703f20e8
Neal Lu <neal.lu06@gmail.com>
2026-04-04 20:11:45 -0700

    finish logic to display musicDisplay.html (tested, not full css)

--- src/main/resources/static/layout/js/homePage.js
+++ src/main/resources/static/layout/js/homePage.js
@@ -239,7 +239,13 @@ function renderCards(items) {
                 : `<div class="rec-img rec-img-placeholder">No Image</div>`;
 
         const mediaType = item.mediaType.toLowerCase();
-        const destination = `/mediaView/${mediaType}/${item.mediaApiId}`;
+        // Adjust the destination URL so backend can fetch the artist and track name correctly for last FM
+        let destination; 
+        if (item.mediaType === 'SONG') {
+          destination = `/mediaView/song/${encodeURIComponent(item.artist)}/${encodeURIComponent(item.title)}`; 
+        } else { 
+          destination = `/mediaView/${mediaType}/${item.mediaApiId}`;
+        } 
         card.href = "javascript:void(0)";
         card.addEventListener("click", async (e) => {
             if (e.target.closest(".rec-btn")) return;
